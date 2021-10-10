eea23f6671578c4332c32936144be58725eb35a1
Gabriel <gabrielcemp@gmail.com>
2021-10-10 16:11:09 -0300

    refactor create product via gRPC method

--- src/modules/products/grpcServer/create-product-grpc-server.controller.ts
+++ src/modules/products/grpcServer/create-product-grpc-server.controller.ts
@@ -2,12 +2,12 @@
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import { Product } from '@prisma/client';
-import { Metadata, ServerUnaryCall, status } from 'grpc';
+import { Metadata, ServerUnaryCall } from 'grpc';
 import { CreateProductDTO } from '../dtos/create-product.dto';
 import { CreateProductService } from '../useCases/createProduct/create-product.service';
 
 @Controller()
-export class ProductGrpcServerController {
+export class CreateProductGrpcServerController {
   constructor(private createProductService: CreateProductService) {}
 
   @GrpcMethod('ProductService')
--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -16,7 +16,7 @@ import { UpdateProductService } from './useCases/updateProduct/update-product.se
 import { RemoveProductController } from './useCases/removeProduct/remove-product.controller';
 import { RemoveProductService } from './useCases/removeProduct/remove-product.service';
 
-import { ProductGrpcServerController } from './grpcServer/product-grpc-server.controller';
+import { CreateProductGrpcServerController } from './grpcServer/create-product-grpc-server.controller';
 import { UpdateProductGrpcServerController } from './grpcServer/update-product-grpc-server.controller';
 import { FindOneProductGrpcServerController } from './grpcServer/find-one-product-grpc-server.controller';
 import { FindAllProductsGrpcServerController } from './grpcServer/find-all-products-grpc-server.controller';
@@ -30,7 +30,7 @@ import { RemoveProductGrpcServerController } from './grpcServer/remove-product-g
     UpdateProductController,
     RemoveProductController,
 
-    ProductGrpcServerController,
+    CreateProductGrpcServerController,
     UpdateProductGrpcServerController,
     FindOneProductGrpcServerController,
     FindAllProductsGrpcServerController,
